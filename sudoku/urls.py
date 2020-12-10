153175eb94b7c9a265e5c5681e709075c7e1cf40
Laura Moon <laura.kolb@gmail.com>
2020-12-10 14:50:31 -0600

    Add files and changes for deployment to heroku

--- sudoku/urls.py
+++ sudoku/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('solver/', include('solver.urls')),
+    path('', RedirectView.as_view(url='solver/', permanent=True))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
